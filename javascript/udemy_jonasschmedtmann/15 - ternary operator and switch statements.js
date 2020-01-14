0fa5a879a432f43f7f2bdb714aadc07cd6bae7ab
Ronald Greeff <12639901+ronaldgreeff@users.noreply.github.com>
2020-01-14 16:20:31 +0000

    15

---
+++ javascript/udemy_jonasschmedtmann/15 - ternary operator and switch statements.js
@@ -0,0 +1,9 @@
+// The Ternary Operator and Switch Statements
+
+var firstName = 'John';
+var age = 16;
+
+age >= 18 ? console.log(firstName + ' drinks beer.') : console.log(firstName + ' drinks juice.');
+// bool // //               if true               // //                if false               //
+
+var drink = 
